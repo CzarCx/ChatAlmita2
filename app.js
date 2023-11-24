14a5da7cad33a4f9c5ea160a7fa9b7d0a84c0fb2
CzarCx <garcia.bautista.cesaremiliano@gmail.com>
2023-11-24 17:33:53 -0600

    holi

---
+++ app.js
@@ -0,0 +1,68 @@
+///web
+
+//const express = require('express');
+//const bodyParser = require('body-parser');
+//const fs = require('fs');
+//const https = require('https');
+//const app = express();
+//var server = https.createServer(app);
+//const io = require('socket.io')(server);'
+
+
+// Configuración para localhost (en este caso, utilizando HTTP)
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var clientes = []; // Lista de clientes conectados
+
+//web tambien
+//const PORT = 3000;
+//server = app.listen(PORT, '0.0.0.0', () => {
+//  console.log(`Servidor iniciado en ${PORT}`);
+//});
+
+// Configuración del servidor y middleware
+app.use(express.static('public')); // Configuración de archivos estáticos
+app.use(bodyParser.urlencoded({ extended: false })); // Configuración de body-parser para manejar solicitudes
+app.use(bodyParser.json());
+server.listen(8080, '0.0.0.0', () => console.log('Servidor iniciado en 8080')); // Iniciando servidor en el puerto 8080
+
+// Rutas para servir archivos estáticos
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/chat/:username', function (req, res) {
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+// Endpoint para iniciar sesión en el chat
+app.post('/login', function (req, res) {
+  let username = req.body.username;
+  let id = req.body.id;
+  clientes.push({ id: id, username: username });
+  io.emit('socket_conectado', { id: id, username: username }); // Emitir evento de nuevo cliente conectado a todos los sockets
+  return res.json(clientes); // Enviar la lista actualizada de clientes al cliente que hace la solicitud
+});
+
+// Endpoint para enviar mensajes en el chat
+app.post('/send', function (req, res) {
+  let username = req.body.username;
+  let id = req.body.id;
+  let msg = req.body.text;
+  io.emit('mensaje', { id: id, msg: msg, username: username }); // Emitir el mensaje a todos los sockets conectados
+  return res.json({ text: 'Mensaje enviado.' }); // Enviar respuesta al cliente que envió el mensaje
+});
+
+// Manejar conexiones de sockets
+io.on('connection', socket => {
+  console.log('Socket conectado', socket.id);
+
+  // Manejar la desconexión de un socket
+  socket.on('disconnect', () => {
+    clientes = clientes.filter(cliente => cliente.id != socket.id); // Filtrar el cliente desconectado
+    io.emit('socket_desconectado', { texto: 'Socket desconectado.', id: socket.id }); // Emitir evento de cliente desconectado a todos los sockets
+  });
+});
---
+++ public/js/logica.js
@@ -0,0 +1,119 @@
+// Establecer la conexión del socket con el servidor
+var socket = io.connect('http://192.168.100.16:8080'); // IP DEL DISPOSITIVO PARA DEPLOYAR EN LOCAL
+
+//const socket = io("ws://urlserverhost.com:3000", { //cambiar url host
+//  transports: ["websocket"]   //WEB
+//});
+
+// Elementos del DOM
+var list = document.querySelector('#lista-users'); // Lista de usuarios
+var username = window.location.pathname.replace('/chat/', ''); // Nombre de usuario obtenido de la URL
+var clientes = []; // Lista de clientes conectados
+
+
+// Función para conectar al chat
+function conectarChat() {
+  var id = socket.id; // Obtener el ID del socket actual
+  console.log('id:', socket.id, 'useranme:', username);
+
+  // Realizar una solicitud POST al servidor para iniciar sesión con el nombre de usuario y el ID del socket
+  $.post('/login', { username: username, id: id }, function (data) {
+    console.log(data);
+    clientes = data; // Actualizar la lista de clientes con la respuesta del servidor
+    list.innerHTML += 'Cargando...';
+    var html = '';
+    // Generar la lista de usuarios conectados para mostrar en la interfaz
+    clientes.forEach(function (cliente) {
+      html += '<li>' + cliente.username + '</li>';
+    });
+    list.innerHTML = html; // Mostrar la lista de usuarios en el DOM
+    $('.loader').hide(); // Ocultar el indicador de carga
+  });
+  
+  // Establecer como conectado y habilitar el campo de entrada y el contenedor de chat
+  isConnected = true;
+  document.getElementById('input').disabled = false;
+  document.getElementById('chat-container').style.display = 'block';
+}
+
+// Función para enviar un mensaje
+function enviarMensaje(e) {
+  if (!isConnected) {
+    return;
+  }
+
+  if (e.which != 13) return; // Verificar si la tecla presionada es 'Enter'
+  var msg = document.querySelector('#input').value; // Obtener el mensaje del campo de entrada
+  if (msg.length <= 0) return; // Verificar si el mensaje está vacío
+
+  // Enviar el mensaje al servidor utilizando una solicitud POST
+  $.post('/send', {
+    text: msg,
+    username: username,
+    id: socket.id
+  }, function (data) {
+    document.querySelector('#input').value = ''; // Limpiar el campo de entrada después de enviar el mensaje
+
+    // Desplazar hacia abajo al contenedor de mensajes para mostrar el nuevo mensaje
+    var mensajesContainer = document.querySelector('.mensajes-container');
+    mensajesContainer.scrollTop = mensajesContainer.scrollHeight;
+  });
+}
+
+// Escuchar el evento 'mensaje' del socket
+socket.on('mensaje', function (data) {
+  // Procesar el mensaje recibido y mostrarlo en la interfaz de usuario
+  data.username = data.username.replace('</', '');
+  var sanitized = data.msg.replace('</', '');
+  sanitized = sanitized.replace('>', '');
+  sanitized = sanitized.replace('<', '');
+  if (data.id == socket.id) {
+    // Mostrar mensajes propios en un estilo específico
+    var msj = `
+      <div class="local-message">
+        <strong>${data.username}: </strong>
+        <p>${sanitized}</p>
+      </div>
+    `;
+    document.querySelector('.mensajes-container').innerHTML += msj;
+  } else {
+    // Mostrar mensajes de otros usuarios en otro estilo
+    var msj = `
+      <div class="remote-message">
+        <strong>${data.username}: </strong>
+        <p>${sanitized}</p>
+      </div>
+    `;
+    document.querySelector('.mensajes-container').innerHTML += msj;
+  }
+});
+
+// Escuchar el evento 'socket_desconectado' del socket
+socket.on('socket_desconectado', function (data) {
+  console.log(data);
+  // Eliminar al cliente desconectado de la lista de clientes
+  clientes = clientes.filter(function (cliente) {
+    return cliente.id != data.id;
+  });
+  list.innerHTML += 'Cargando...';
+  var html = '';
+  // Actualizar la lista de usuarios conectados en el DOM
+  clientes.forEach(function (cliente) {
+    html += '<li>' + cliente.username + '</li>';
+  });
+  list.innerHTML = html;
+});
+
+// Escuchar el evento 'socket_conectado' del socket
+socket.on('socket_conectado', function (data) {
+  console.log(data);
+  // Agregar al cliente recién conectado a la lista de clientes
+  clientes.push(data);
+  list.innerHTML += 'Cargando...';
+  var html = '';
+  // Actualizar la lista de usuarios conectados en el DOM
+  clientes.forEach(function (cliente) {
+    html += '<li>' + cliente.username + '</li>';
+  });
+  list.innerHTML = html;
+});
